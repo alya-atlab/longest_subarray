4f7985c95b9922d155bc0f12b7c09e4717b5aa8b
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-06 13:03:55 +0200

    Create The Function

--- longest_subarray.py
+++ longest_subarray.py
@@ -1,2 +1,15 @@
 def longest_subarray(array):
-    
+    zeros_count = 0
+    ones_count = 0
+    for i in array:
+        if i == 0:
+            zeros_count += 1
+        else:
+            ones_count += 1
+    if zeros_count <= ones_count:
+        return zeros_count * 2
+    else:
+        return ones_count * 2
+
+
+print(longest_subarray([1, 1, 1, 1, 1, 0, 0, 0, 0, 0]))
