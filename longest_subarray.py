3f44758f5cc930542a3541c88c5d2d1d7d22fa97
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-14 16:09:00 +0200

    Add comments

--- longest_subarray.py
+++ longest_subarray.py
@@ -6,10 +6,12 @@ def longest_subarray(array):
             zeros_count += 1
         else:
             ones_count += 1
-    if zeros_count <= ones_count:
+    if (
+        zeros_count <= ones_count
+    ):  # the logest subarray is when the zero and the one is equal so we return the small count times two
         return zeros_count * 2
     else:
         return ones_count * 2
 
 
-print(longest_subarray([1, 1, 1, 1, 1, 0, 0, 0, 0, 0]))
+print(longest_subarray([1, 0, 1, 0, 0, 0, 1, 0, 0]))
